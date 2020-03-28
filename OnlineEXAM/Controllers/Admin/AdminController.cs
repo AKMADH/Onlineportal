13c650613a4a4bb7365b74d2d87e0f41a1132593
Anish kumar Mishra <akmadh2013cse@gmail.com>
2020-03-29 03:02:01 +0530

    initiall commit

---
+++ OnlineEXAM/Controllers/Admin/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using OnlineEXAM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineEXAM.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        private onlineTestEntities db = new onlineTestEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+    }
+}
---
+++ OnlineEXAM/Controllers/UsersController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineEXAM.Models;
+
+namespace OnlineEXAM.Controllers
+{
+    public class UsersController : Controller
+    {
+        private onlineTestEntities db = new onlineTestEntities();
+
+        // GET: Users
+       
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FullName,ProfilImage,Email,Password,Mobile")] User user, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (file != null)
+                {
+                    string filename = Path.GetFileName(file.FileName);
+                    user.ProfilImage = filename;
+                    string filepath = Path.Combine(Server.MapPath("~/profilepic/user/"), filename);
+                    file.SaveAs(filepath);
+                }
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,FullName,ProfilImage,Email,Password,Mobile")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
